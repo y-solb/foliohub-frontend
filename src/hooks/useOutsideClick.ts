f877f4ca907b8d7c88cae3566aa21fc49af6e521
y-solb <ysolb99@gmail.com>
2024-01-30 18:58:36 +0900

    Feat: useOutsideClick hook 추가

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,30 @@
+import { useEffect, useRef, useState } from 'react'
+
+export default function useOutsideClick(
+  callback: () => void,
+  defaultValue = false,
+): [
+  boolean,
+  React.Dispatch<React.SetStateAction<boolean>>,
+  React.RefObject<HTMLDivElement>,
+] {
+  const ref = useRef<HTMLDivElement>(null)
+  const [isOpen, setIsOpen] = useState(defaultValue)
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback()
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [callback])
+
+  return [isOpen, setIsOpen, ref]
+}
