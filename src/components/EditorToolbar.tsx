007982aceaa446e899884fe38088db4c78957244
y-solb <ysolb99@gmail.com>
2024-02-12 23:42:11 +0900

    Feat: ql-color 선택된 색상에 active 안 되는 부분 수정

--- src/components/EditorToolbar.tsx
+++ src/components/EditorToolbar.tsx
@@ -83,25 +83,25 @@ export function QuillToolbar() {
       </span>
       <span className="ql-formats">
         <select className="ql-color" aria-label="ql-color">
-          <option value="#00000" aria-label="ql-color-black" />
-          <option value="#6B7280" aria-label="ql-color-gray" />
-          <option value="#F43F5E" aria-label="ql-color-red" />
-          <option value="#F97316" aria-label="ql-color-orange" />
-          <option value="#FACC15" aria-label="ql-color-yellow" />
-          <option value="#84CC16" aria-label="ql-color-green" />
+          <option value="#000000" aria-label="ql-color-black" />
+          <option value="#6b7280" aria-label="ql-color-gray" />
+          <option value="#ef4444" aria-label="ql-color-red" />
+          <option value="#f97316" aria-label="ql-color-orange" />
+          <option value="#facc15" aria-label="ql-color-yellow" />
+          <option value="#84cc16" aria-label="ql-color-green" />
           <option value="#0ea4e9" aria-label="ql-color-sky" />
-          <option value="#6366F1" aria-label="ql-color-blue" />
-          <option value="#8B5CF6" aria-label="ql-color-purple" />
+          <option value="#6366f1" aria-label="ql-color-navy" />
+          <option value="#8b5cf6" aria-label="ql-color-purple" />
         </select>
         <select className="ql-background" aria-label="ql-background">
           <option value="#ffffff" aria-label="ql-background-white" />
           <option value="#a7a9ac" aria-label="ql-background-gray" />
-          <option value="#f79bae" aria-label="ql-background-red" />
+          <option value="#f77a7a" aria-label="ql-background-red" />
           <option value="#fcc288" aria-label="ql-background-orange" />
           <option value="#fcf3b1" aria-label="ql-background-yellow" />
           <option value="#c7ebc1" aria-label="ql-background-green" />
           <option value="#b8dffc" aria-label="ql-background-sky" />
-          <option value="#c6cfff" aria-label="ql-background-blue" />
+          <option value="#c6cfff" aria-label="ql-background-navy" />
           <option value="#d6d2f5" aria-label="ql-background-purple" />
         </select>
       </span>
