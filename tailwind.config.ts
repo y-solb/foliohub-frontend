1cf807b7340e06e3a7b3f6cf1dca73621c20d017
y-solb <ysolb99@gmail.com>
2024-02-13 15:34:42 +0900

    Design: animation에 fadeIn, fadeOut 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    animation: {
+      fadeIn: 'fadeIn 1s ease-out',
+      fadeOut: 'fadeOut 1s ease-out',
+    },
+    keyframes: {
+      fadeIn: {
+        '0%': {
+          opacity: '0',
+        },
+        '100%': {
+          opacity: '1',
+        },
+      },
+      fadeOut: {
+        '0%': {
+          opacity: '1',
+        },
+        '100%': {
+          opacity: '0',
+        },
       },
     },
+    extend: {},
   },
   plugins: [],
-};
-export default config;
+}
+export default config
