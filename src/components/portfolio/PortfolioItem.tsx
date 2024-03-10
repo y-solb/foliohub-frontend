dce9a1597097a983b74216d6420d0b8077e65c4b
y-solb <ysolb99@gmail.com>
2024-03-10 11:52:09 +0900

    Feat: PortFolioItem, PortfolioItemSkeleton component 추가

---
+++ src/components/portfolio/PortfolioItem.tsx
@@ -0,0 +1,48 @@
+import { PortfolioItem } from '@/hooks/queries/portfolio'
+import Image from 'next/image'
+import Link from 'next/link'
+import { IoMdHeart } from 'react-icons/io'
+
+interface PortFolioItemProps {
+  portfolio: PortfolioItem
+}
+
+function PortFolioItem({
+  portfolio: {
+    id,
+    userId,
+    userJob,
+    thumbnail,
+    displayName,
+    shortBio,
+    likeCount,
+  },
+}: PortFolioItemProps) {
+  return (
+    <li className="grid-item-wrapper">
+      <Link
+        href={`/${userId}`}
+        className="relative flex flex-col items-center px-10 py-8 w-full h-full"
+      >
+        <div className="relative flex min-w-32 min-h-32 rounded-full border border-solid border-gray-100 shadow-md overflow-hidden">
+          {thumbnail && (
+            <Image src={thumbnail} alt={`image_${id}`} priority fill />
+          )}
+        </div>
+        <div className="w-full flex flex-col items-center mt-4 gap-2 h-20">
+          <h2>{displayName}</h2>
+          <p className="body2 text-gray-400 ellipsis2">{shortBio}</p>
+        </div>
+        <div className="flex items-center min-h-8 px-4 rounded-3xl text-gray-400 border border-solid border-gray-200 body2 font-medium mt-6">
+          {userJob}
+        </div>
+        <div className="absolute bottom-3 right-6 flex gap-1 items-center justify-end">
+          <IoMdHeart size={16} className="text-gray-300" />
+          <span className="text-gray-300 body2">{likeCount}</span>
+        </div>
+      </Link>
+    </li>
+  )
+}
+
+export default PortFolioItem
---
+++ src/components/portfolio/PortfolioItemSkeleton.tsx
@@ -0,0 +1,19 @@
+import SkeletonText from '../common/SkeletonText'
+
+function PortfolioItemSkeleton() {
+  return (
+    <li className="grid-item-wrapper">
+      <div className="animate-pulse relative flex flex-col items-center px-10 py-8 w-full h-full">
+        <div className="relative min-w-32 min-h-32 rounded-full border border-solid border-gray-100 bg-gray-100" />
+        <div className="w-full flex flex-col items-center mt-4 gap-2 h-20">
+          <SkeletonText variant="h2" width="30%" />
+          <SkeletonText variant="body2" />
+          <SkeletonText variant="body2" />
+        </div>
+        <div className="flex items-center bg-gray-100 w-32 h-8 px-4 rounded-3xl text-gray-400 mt-6" />
+      </div>
+    </li>
+  )
+}
+
+export default PortfolioItemSkeleton
