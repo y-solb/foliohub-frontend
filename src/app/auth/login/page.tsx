46b3648c7f19301378eed79b2e1da2ff6a3a185e
y-solb <ysolb99@gmail.com>
2024-03-12 18:31:50 +0900

    Remove: 로그인 모달로 대체

--- src/app/auth/login/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function LoginPage() {
-  return (
-    <div>
-      <a href={`${process.env.NEXT_PUBLIC_API_HOST}/v1/auth/redirect/google`}>
-        Google 로그인
-      </a>
-    </div>
-  )
-}
