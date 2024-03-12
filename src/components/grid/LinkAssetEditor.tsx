ac46d2f8263f3685503237926f6f06be6fe54095
y-solb <ysolb99@gmail.com>
2024-03-12 14:08:40 +0900

    Feat: LinkAsset 관련 에러처리 추가

--- src/components/grid/LinkAssetEditor.tsx
+++ src/components/grid/LinkAssetEditor.tsx
@@ -3,6 +3,7 @@ import useOutsideClick from '@/hooks/useOutsideClick'
 import { useState } from 'react'
 import { RxLink2 } from 'react-icons/rx'
 import { useMetadataQuery } from '@/hooks/queries/metadata'
+import { MdError } from 'react-icons/md'
 import DeleteGridItemButton from '../DeleteGridItemButton'
 import InputToolbar from '../toolbar/InputToolbar'
 
@@ -23,7 +24,7 @@ function LinkAssetEditor({
 }: LinkAssetEditorProps) {
   const { value, id } = asset
 
-  const { data } = useMetadataQuery(value.link)
+  const { data, isSuccess } = useMetadataQuery(value.link)
 
   const [isOpenControl, setIsOpenControl] = useState(false)
   const [activeTool, setActiveTool] = useState('')
@@ -65,37 +66,49 @@ function LinkAssetEditor({
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      <div
-        className={`relative ${data?.image ? 'grid' : ''} flex-1 grid-item-wrapper overflow-hidden`}
-        style={{
-          gridTemplateColumns:
-            width > height ? `${height}fr ${width - height}fr` : '',
-          gridTemplateRows:
-            width < height ? `${height}fr ${width - height}fr` : '1fr 1fr',
-        }}
-      >
-        {data?.image && (
-          <div
-            className={`relative w-full overflow-hidden ${width !== height ? 'pb-[100%]' : ''}`}
-          >
-            <img
-              src={data?.image}
-              alt={`image_${id}`}
-              className="absolute top-0 object-cover w-full h-full"
-            />
-          </div>
-        )}
-        <div className="flex flex-col gap-2 p-3 overflow-hidden">
-          <p className="body2 ellipsis2">{data?.title}</p>
-          {(width > 1 || height > 1) && (
-            <p
-              className={`body3 text-gray-400  ${width > 2 || height > 2 ? 'ellipsis5' : 'ellipsis3'}`}
+      {isSuccess ? (
+        <div
+          className={`relative ${data?.image ? 'grid' : ''} flex-1 grid-item-wrapper overflow-hidden`}
+          style={{
+            gridTemplateColumns:
+              width > height ? `${height}fr ${width - height}fr` : '',
+            gridTemplateRows:
+              width < height ? `${height}fr ${width - height}fr` : '1fr 1fr',
+          }}
+        >
+          {data?.image && (
+            <div
+              className={`relative w-full overflow-hidden ${width !== height ? 'pb-[100%]' : ''}`}
             >
-              {data?.description}
-            </p>
+              <img
+                src={data?.image}
+                alt={`image_${id}`}
+                className="absolute top-0 object-cover w-full h-full"
+              />
+            </div>
           )}
+          <div className="flex flex-col gap-2 p-3 overflow-hidden">
+            <p className="body2 ellipsis2">{data?.title}</p>
+            {(width > 1 || height > 1) && (
+              <p
+                className={`body3 text-gray-400  ${width > 2 || height > 2 ? 'ellipsis5' : 'ellipsis3'}`}
+              >
+                {data?.description}
+              </p>
+            )}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="flex flex-col justify-center items-center gap-4 grid-item-wrapper overflow-y-auto p-4">
+          <div>
+            <MdError size={24} />
+          </div>
+          <p className="body2 text-gray-400">
+            데이터를 불러오는 도중 문제가 발생했어요! 링크를 올바르게 입력했는지
+            확인해 주세요.
+          </p>
+        </div>
+      )}
       {isOpenControl && (
         <div className="control-wrapper">
           <DeleteGridItemButton
