8f892d15e5ee1752dd754d573c1108e58a7e4531
y-solb <ysolb99@gmail.com>
2024-03-14 15:39:48 +0900

    Design: animate-pulse 위치변경

--- src/components/HeaderSkeleton.tsx
+++ src/components/HeaderSkeleton.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 function HeaderSkeleton() {
   return (
-    <div className="animate-pulse flex items-center justify-between lg:px-20 px-10 h-16">
+    <div className=" flex items-center justify-between lg:px-20 px-10 h-16">
       <div className="flex items-center justify-center gap-2">
         <Image
           src="/foliohub_text.svg"
@@ -11,7 +11,7 @@ function HeaderSkeleton() {
           height={32}
         />
       </div>
-      <div className="w-10 h-10 bg-gray-100 rounded-full" />
+      <div className="animate-pulse w-10 h-10 bg-gray-100 rounded-full" />
     </div>
   )
 }
