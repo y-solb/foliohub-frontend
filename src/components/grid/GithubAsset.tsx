9344d1b4e7fc95b668b9cc7e90549d54594f2987
y-solb <ysolb99@gmail.com>
2024-03-18 12:57:42 +0900

    Feat: 링크 이동할 수 있도록 기능 추가

--- src/components/grid/GithubAsset.tsx
+++ src/components/grid/GithubAsset.tsx
@@ -1,5 +1,6 @@
 import { AssetType } from '@/types'
 import GitHubCalendar from 'react-github-calendar'
+import Link from 'next/link'
 
 type Activity = {
   date: string
@@ -28,21 +29,28 @@ function GithubAsset({ asset, width }: GithubAssetProps) {
   }
 
   return (
-    <div className="relative flex flex-1">
-      <div className="github-calendar-wrapper relative flex flex-1 grid-item-wrapper overflow-hidden p-4 justify-center items-center">
-        <GitHubCalendar
-          username={value.githubId}
-          transformData={calculateContributions}
-          colorScheme="light"
-          throwOnError={false}
-          errorMessage="GitHub를 불러오는 도중 문제가 발생했습니다. 문제가 지속되는 경우 GitHub 계정을 올바르게 입력했는지 확인해 주세요."
-          blockRadius={2}
-          blockMargin={2}
-          blockSize={6}
-          hideColorLegend
-          hideMonthLabels
-          hideTotalCount
-        />
+    <div className="relative flex flex-1 w-full max-width-full">
+      <div className="relative flex flex-1 grid-item-wrapper overflow-hidden">
+        <Link
+          href={`https://github.com/${value.githubId}`}
+          className="github-calendar-wrapper relative flex flex-1 justify-center items-center p-4"
+          aria-label="githubLink"
+          target="_blank"
+        >
+          <GitHubCalendar
+            username={value.githubId}
+            transformData={calculateContributions}
+            colorScheme="light"
+            throwOnError={false}
+            errorMessage="GitHub를 불러오는 도중 문제가 발생했습니다. 문제가 지속되는 경우 GitHub 계정을 올바르게 입력했는지 확인해 주세요."
+            blockRadius={2}
+            blockMargin={2}
+            blockSize={6}
+            hideColorLegend
+            hideMonthLabels
+            hideTotalCount
+          />
+        </Link>
       </div>
     </div>
   )
--- src/components/grid/LinkAsset.tsx
+++ src/components/grid/LinkAsset.tsx
@@ -1,6 +1,7 @@
 import { AssetType } from '@/types'
 import { MdError } from 'react-icons/md'
 import { useMetadataQuery } from '@/hooks/queries/metadata'
+import Link from 'next/link'
 
 interface LinkAssetProps {
   asset: AssetType
@@ -14,36 +15,41 @@ function LinkAsset({ asset, width, height }: LinkAssetProps) {
   return (
     <div className="relative flex flex-1 w-full max-width-full">
       {isSuccess && (
-        <div
-          className={`relative ${data?.image ? 'grid' : ''} flex-1 grid-item-wrapper overflow-hidden`}
-          style={{
-            gridTemplateColumns:
-              width > height ? `${height}fr ${width - height}fr` : '',
-            gridTemplateRows:
-              width < height ? `${height}fr ${width - height}fr` : '1fr 1fr',
-          }}
-        >
-          {data?.image && (
-            <div
-              className={`relative w-full overflow-hidden ${width !== height ? 'pb-[100%]' : ''}`}
-            >
-              <img
-                src={data?.image}
-                alt={`image_${id}`}
-                className="absolute top-0 object-cover w-full h-full"
-              />
-            </div>
-          )}
-          <div className="flex flex-col gap-2 p-3 overflow-hidden">
-            <p className="body2 ellipsis2">{data?.title}</p>
-            {(width > 1 || height > 1) && (
-              <p
-                className={`body3 text-gray-400  ${width > 2 || height > 2 ? 'ellipsis5' : 'ellipsis3'}`}
+        <div className="relative flex-1 grid-item-wrapper overflow-hidden">
+          <Link
+            href={`${value.link}`}
+            className={`relative ${data?.image ? 'grid' : ''} w-full h-full overflow-hidden`}
+            style={{
+              gridTemplateColumns:
+                width > height ? `${height}fr ${width - height}fr` : '',
+              gridTemplateRows:
+                width < height ? `${height}fr ${width - height}fr` : '1fr 1fr',
+            }}
+            aria-label="link"
+            target="_blank"
+          >
+            {data?.image && (
+              <div
+                className={`relative w-full ${width !== height ? 'pb-[100%]' : ''}`}
               >
-                {data?.description}
-              </p>
+                <img
+                  src={data?.image}
+                  alt={`image_${id}`}
+                  className="absolute top-0 object-cover w-full h-full"
+                />
+              </div>
             )}
-          </div>
+            <div className="flex flex-col gap-2 p-3 overflow-hidden">
+              <p className="body2 ellipsis2">{data?.title}</p>
+              {(width > 1 || height > 1) && (
+                <p
+                  className={`body3 text-gray-400  ${width > 2 || height > 2 ? 'ellipsis5' : 'ellipsis3'}`}
+                >
+                  {data?.description}
+                </p>
+              )}
+            </div>
+          </Link>
         </div>
       )}
       {isError && (
