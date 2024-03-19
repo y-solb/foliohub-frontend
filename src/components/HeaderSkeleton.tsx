aa34e2acbbcbcf906ca0f4bd89a507dbeb08f743
y-solb <ysolb99@gmail.com>
2024-03-19 21:33:57 +0900

    Design: Header와 동일하게 fixed 추가

--- src/components/HeaderSkeleton.tsx
+++ src/components/HeaderSkeleton.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 function HeaderSkeleton() {
   return (
-    <div className=" flex items-center justify-between lg:px-20 px-10 h-16">
+    <div className="fixed op-0 left-0 z-10 w-full flex items-center justify-between lg:px-20 px-10 h-16">
       <div className="flex items-center justify-center gap-2">
         <Image
           src="/foliohub_text_logo.svg"
