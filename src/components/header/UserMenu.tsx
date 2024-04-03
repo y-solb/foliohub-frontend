a6f60d5077147b3d4f81cef0f5d9eea296ac37f0
y-solb <ysolb99@gmail.com>
2024-04-03 17:56:16 +0900

    Fix: 로그아웃 후 /로 이동 및 alert 추가

--- src/components/header/UserMenu.tsx
+++ src/components/header/UserMenu.tsx
@@ -8,6 +8,8 @@ import { useLogoutMutation } from '@/hooks/queries/auth'
 import { BiLogOutCircle, BiUser, BiPencil } from 'react-icons/bi'
 import { useSetRecoilState } from 'recoil'
 import authInfoState from '@/recoil/atoms/authInfoState'
+import useOpenAlertModal from '@/hooks/useOpenAlertModal'
+import { useRouter } from 'next/navigation'
 import EmptyThumbnail from '../common/EmptyThumbnail'
 
 interface UserMenuProps {
@@ -15,8 +17,10 @@ interface UserMenuProps {
 }
 
 function UserMenu({ authInfo: { id, username, thumbnail } }: UserMenuProps) {
+  const router = useRouter()
   const setAuthInfo = useSetRecoilState(authInfoState)
   const { mutate } = useLogoutMutation()
+  const { openAlert } = useOpenAlertModal()
   const [isOpenNav, setIsOpenNav, outRef] = useOutsideClick<HTMLDivElement>(
     () => {
       setIsOpenNav(false)
@@ -29,7 +33,9 @@ function UserMenu({ authInfo: { id, username, thumbnail } }: UserMenuProps) {
 
   const handleLogout = () => {
     mutate()
+    router.push('/')
     setAuthInfo(null)
+    openAlert('로그아웃이 완료되었어요!', '다음에 다시 만나요 👋')
   }
 
   return (
@@ -57,30 +63,30 @@ function UserMenu({ authInfo: { id, username, thumbnail } }: UserMenuProps) {
           className="absolute right-0 -bottom-[11.5rem] bg-white w-52 border border-solid border-gray-200 rounded-2xl"
         >
           <ul className="py-2">
-            <li className="px-6 py-2 hover:bg-gray-100">
+            <li className="hover:bg-gray-100">
               <Link
                 href="/mypage"
-                className="flex items-center gap-6 h-8 text-black body2"
+                className="flex items-center gap-6 w-full min-h-8 text-black body2 px-6 py-2"
               >
                 <BiUser size={24} />
                 <span>마이페이지</span>
               </Link>
             </li>
-            <li className="px-6 py-2 hover:bg-gray-100">
+            <li className="hover:bg-gray-100">
               <Link
                 href={`/edit/${username}`}
-                className="flex items-center gap-6 h-8 text-black body2"
+                className="flex items-center gap-6 w-full min-h-8 text-black body2 px-6 py-2"
               >
                 <BiPencil size={24} />
                 <span>포트폴리오 수정</span>
               </Link>
             </li>
             <div className="h-[1px] bg-gray-200 mx-6 my-2" />
-            <li className="px-6 py-2 hover:bg-gray-100">
+            <li className="hover:bg-gray-100">
               <button
                 type="button"
                 onClick={handleLogout}
-                className="flex items-center gap-6 h-8 text-black body2"
+                className="flex items-center gap-6 w-full min-h-8 text-black body2 px-6 py-2"
               >
                 <BiLogOutCircle size={24} />
                 <span>로그아웃</span>
