d7a4d4ecefb6d9c7c1719744c3aa27c0508acbc1
y-solb <ysolb99@gmail.com>
2024-04-05 23:29:45 +0900

    Design: not found 페이지에 margin 변경

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,10 +9,11 @@ export const metadata: Metadata = {
 export default function NotFound() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center gap-4 mt-28">
+      <div className="flex flex-col items-center justify-center mt-32">
         <h1>404 ERROR</h1>
-        <p className="body1 text-gray-400">해당 페이지를 찾을 수 없어요!</p>
-
+        <p className="body1 text-gray-400 mt-4 mb-10">
+          해당 페이지를 찾을 수 없어요!
+        </p>
         <a
           href="/"
           className="flex items-center justify-center rounded-full bg-black text-white h-12 px-8"
