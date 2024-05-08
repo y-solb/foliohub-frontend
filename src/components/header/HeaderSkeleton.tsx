4fc9089d5c9713c435c931c38c7bb8385b13f56f
y-solb <ysolb99@gmail.com>
2024-05-08 17:29:11 +0900

    Fix: top-0 오타 수정

--- src/components/header/HeaderSkeleton.tsx
+++ src/components/header/HeaderSkeleton.tsx
@@ -2,7 +2,7 @@ import Logo from '../common/Logo'
 
 function HeaderSkeleton() {
   return (
-    <div className="fixed op-0 left-0 z-10 w-full flex items-center justify-between lg:px-20 px-10 h-16">
+    <div className="fixed top-0 left-0 z-10 w-full flex items-center justify-between lg:px-20 px-10 h-16">
       <div className="flex items-center justify-center gap-2">
         <Logo />
       </div>
