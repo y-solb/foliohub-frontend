d116fd33f283d08690ed465b2726a420852fd294
y-solb <ysolb99@gmail.com>
2024-05-08 16:31:53 +0900

    Remove: dehydrate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
 import BaseLayout from '@/components/layout/BaseLayout'
 import RecentPortfolioList from '@/containers/main/RecentPortfolioList'
-import { getPortfolioList } from '@/hooks/queries/portfolio'
-import {
-  HydrationBoundary,
-  QueryClient,
-  dehydrate,
-} from '@tanstack/react-query'
+// import { getPortfolioList } from '@/hooks/queries/portfolio'
+// import {
+//   HydrationBoundary,
+//   QueryClient,
+//   dehydrate,
+// } from '@tanstack/react-query'
 
 export default async function Home() {
-  const queryClient = new QueryClient()
-  await queryClient.prefetchInfiniteQuery({
-    queryKey: ['portfolioList'],
-    queryFn: ({ pageParam }) => getPortfolioList(pageParam as number),
-    initialPageParam: 1,
-  })
-  const dehydratedState = dehydrate(queryClient)
+  // const queryClient = new QueryClient()
+  // await queryClient.prefetchInfiniteQuery({
+  //   queryKey: ['portfolioList'],
+  //   queryFn: ({ pageParam }) => getPortfolioList(pageParam as number),
+  //   initialPageParam: 1,
+  // })
+  // const dehydratedState = dehydrate(queryClient)
   return (
     <BaseLayout>
-      <HydrationBoundary state={dehydratedState}>
-        <RecentPortfolioList />
-      </HydrationBoundary>
+      {/* <HydrationBoundary state={dehydratedState}> */}
+      <RecentPortfolioList />
+      {/* </HydrationBoundary> */}
     </BaseLayout>
   )
 }
