572e94a32163a69d7c42b9979dcb063cae2e0a2b
y-solb <ysolb99@gmail.com>
2024-05-08 17:59:55 +0900

    Refactor: isLoading 제거

--- src/components/portfolio/PortfolioList.tsx
+++ src/components/portfolio/PortfolioList.tsx
@@ -7,28 +7,28 @@ import { PortfolioItemType } from '@/types'
 import PortfolioItemSkeleton from './PortfolioItemSkeleton'
 
 interface PortfolioListProps {
-  isLoading?: boolean
+  // isLoading?: boolean
   isFetching: boolean
   portfolios: PortfolioItemType[]
   loaderRef: React.RefObject<HTMLDivElement>
 }
 
 function PortfolioList({
-  isLoading,
+  // isLoading,
   isFetching,
   portfolios,
   loaderRef,
 }: PortfolioListProps) {
-  if (isLoading)
-    return (
-      <ul className="grid gap-6 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 lg:px-20 px-10">
-        {Array(12)
-          .fill(0)
-          .map((_, index) => (
-            <PortfolioItemSkeleton key={index} />
-          ))}
-      </ul>
-    )
+  // if (isLoading)
+  //   return (
+  //     <ul className="grid gap-6 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 lg:px-20 px-10">
+  //       {Array(12)
+  //         .fill(0)
+  //         .map((_, index) => (
+  //           <PortfolioItemSkeleton key={index} />
+  //         ))}
+  //     </ul>
+  //   )
 
   return (
     <>
