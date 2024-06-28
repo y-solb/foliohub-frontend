e9f014d0bd644c4ad8c54baf4a6b02b221466981
y-solb <ysolb99@gmail.com>
2024-06-29 02:04:54 +0900

    Feat: ProgressBar 스토리북 추가

---
+++ src/components/common/ProgressBar/index.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { RecoilRoot, useSetRecoilState } from 'recoil'
+import progressBarState from '@/recoil/atoms/progressBarState'
+import { useEffect } from 'react'
+import ProgressBar from '.'
+
+const meta: Meta<typeof ProgressBar> = {
+  title: 'Components/ProgressBar',
+  component: ProgressBar,
+  parameters: {
+    componentSubtitle: 'ProgressBar는 로딩 컴포넌트입니다.',
+  },
+  decorators: [
+    (Story) => (
+      <RecoilRoot>
+        <Story />
+      </RecoilRoot>
+    ),
+  ],
+}
+
+export default meta
+
+type Story = StoryObj<typeof ProgressBar>
+
+function ProgressBarWithLoading() {
+  const setIsLoading = useSetRecoilState(progressBarState)
+
+  useEffect(() => {
+    setIsLoading(true)
+  }, [])
+
+  return <ProgressBar />
+}
+
+export const Default: Story = {
+  render: () => <ProgressBarWithLoading />,
+}
