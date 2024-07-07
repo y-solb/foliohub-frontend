2eb5ee98a54c8daad48bb9b33bdea7028b4172ea
y-solb <ysolb99@gmail.com>
2024-07-07 21:26:42 +0900

    Feat: 설문 관련 폼 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import BaseLayout from '@/components/layout/BaseLayout'
 import RecentPortfolioList from '@/containers/main/RecentPortfolioList'
 import { getPortfolioList } from '@/services/portfolio'
+import { IoChatbubblesOutline } from 'react-icons/io5'
 import {
   HydrationBoundary,
   QueryClient,
   dehydrate,
 } from '@tanstack/react-query'
+import ButtonLink from '@/components/common/ButtonLink'
 
 export default async function Home() {
   const queryClient = new QueryClient()
@@ -20,6 +22,13 @@ export default async function Home() {
     <BaseLayout>
       <HydrationBoundary state={dehydratedState}>
         <RecentPortfolioList />
+        <ButtonLink
+          href="https://docs.google.com/forms/d/e/1FAIpQLSf_FDChmWIm80RdOI_KIdg1OuUirjRGwklAXe89zpKclqH_Zg/viewform?usp=sf_link"
+          target="_blank"
+          className="fixed right-4 bottom-4 round-full w-14 h-14 p-0"
+        >
+          <IoChatbubblesOutline size={24} />
+        </ButtonLink>
       </HydrationBoundary>
     </BaseLayout>
   )
