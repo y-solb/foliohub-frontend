3ea86251dc89ef9b9e4a278ae6fd5eeacbb427ad
y-solb <ysolb99@gmail.com>
2024-07-07 22:25:22 +0900

    Chore: test

--- src/containers/main/RecentPortfolioList.tsx
+++ src/containers/main/RecentPortfolioList.tsx
@@ -2,25 +2,24 @@
 
 import PortfolioList from '@/components/portfolio/PortfolioList'
 import { useInfinitePortfolioQuery } from '@/hooks/queries/portfolio'
-import useInfiniteScroll from '@/hooks/useInfiniteScroll'
+// import useInfiniteScroll from '@/hooks/useInfiniteScroll'
 import { useMemo, useRef } from 'react'
 
 function RecentPortfolioList() {
-  const { data, fetchNextPage, isFetchingNextPage, hasNextPage } =
-    useInfinitePortfolioQuery()
+  const { data, isFetchingNextPage } = useInfinitePortfolioQuery()
 
   const portfolios = useMemo(() => {
     return [...(data?.pages?.flatMap((page) => page.data) || [])]
   }, [data])
 
-  const fetchMorePortfolio = () => {
-    if (!isFetchingNextPage && hasNextPage) {
-      fetchNextPage()
-    }
-  }
+  // const fetchMorePortfolio = () => {
+  //   if (!isFetchingNextPage && hasNextPage) {
+  //     fetchNextPage()
+  //   }
+  // }
   const loaderRef = useRef<HTMLDivElement>(null)
 
-  useInfiniteScroll(loaderRef, fetchMorePortfolio)
+  // useInfiniteScroll(loaderRef, fetchMorePortfolio)
 
   return (
     <PortfolioList
