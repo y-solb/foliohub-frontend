f3fb266e9b193446f0e5cceee5f5a7c8522312df
y-solb <ysolb99@gmail.com>
2024-07-23 14:35:34 +0900

    Refactor: CardContent로 분리 및 hover 추가

--- src/components/asset/CardAsset/index.tsx
+++ src/components/asset/CardAsset/index.tsx
@@ -10,110 +10,84 @@ interface CardAssetProps {
   breakpoint: BreakpointType
 }
 
+const getGridTemplate = (width: number, height: number) => ({
+  gridTemplateColumns: width > height ? `${height}fr ${width - height}fr` : '',
+  gridTemplateRows:
+    // eslint-disable-next-line no-nested-ternary
+    width < height
+      ? `${height}fr ${height - width}fr`
+      : width === height
+        ? '1fr 1fr'
+        : '',
+})
+
+function CardContent({ asset, width, height, breakpoint }: CardAssetProps) {
+  const { id, value } = asset
+  return (
+    <div
+      className={`relative flex-1 ${value?.imageUrl ? 'grid' : ''}  grid-item-wrapper overflow-hidden p-3 gap-2`}
+      style={getGridTemplate(width, height)}
+    >
+      {value?.imageUrl && (
+        <div className="relative w-full h-full rounded-xl overflow-hidden">
+          <Image
+            src={value?.imageUrl}
+            className="object-cover"
+            alt={`image_${id}`}
+            style={{
+              objectPosition: `${value.pos?.[breakpoint] ? value.pos[breakpoint].x : 50}% ${value.pos?.[breakpoint] ? value.pos[breakpoint].y : 50}%`,
+            }}
+            quality={100}
+            fill
+            priority
+            sizes="(max-width: 842px) 100vw, 80vw"
+          />
+          {value.link && (
+            <div
+              aria-label="related-image-link"
+              className="image-link absolute bottom-2 left-2 flex rounded-full border border-solid border-gray-100 bg-white shadow-md p-1 transition-all group-hover:rotate-90 group-hover:scale-110"
+            >
+              <TbLink size={20} />
+            </div>
+          )}
+        </div>
+      )}
+      <div className="flex flex-col gap-2 overflow-x-hidden	overflow-y-auto">
+        {value?.title && <p className="body2 font-medium">{value?.title}</p>}
+        {value?.description && (
+          <p className="body3 text-gray-400">{value?.description}</p>
+        )}
+      </div>
+    </div>
+  )
+}
+
 function CardAsset({ asset, width, height, breakpoint }: CardAssetProps) {
-  const { value, id } = asset
+  const { value } = asset
 
   return (
-    <div className="relative flex flex-1 w-full max-width-full">
+    <div className="relative flex flex-1 w-full max-width-full group">
       {value.link ? (
         <Link
-          href={`${value.link}`}
-          className={`relative flex-1 ${value?.imageUrl ? 'grid' : ''}  grid-item-wrapper overflow-hidden p-3 gap-2`}
-          style={{
-            gridTemplateColumns:
-              width > height ? `${height}fr ${width - height}fr` : '',
-            gridTemplateRows:
-              // eslint-disable-next-line no-nested-ternary
-              width < height
-                ? `${height}fr ${height - width}fr`
-                : width === height
-                  ? '1fr 1fr'
-                  : '',
-          }}
-          aria-label="link"
+          href={value.link}
+          className="flex flex-1 w-full max-width-full"
           target="_blank"
+          rel="noopener noreferrer"
         >
-          {value?.imageUrl && (
-            <div
-              className={`relative w-full rounded-xl overflow-hidden ${width !== height ? 'h-full' : ''}`}
-            >
-              <Image
-                src={value?.imageUrl}
-                className="object-cover"
-                alt={`image_${id}`}
-                style={{
-                  objectPosition: `${value.pos?.[breakpoint] ? value.pos[breakpoint].x : 50}% ${value.pos?.[breakpoint] ? value.pos[breakpoint].y : 50}%`,
-                }}
-                quality={100}
-                fill
-                priority
-                sizes="(max-width: 842px) 100vw, 80vw"
-              />
-              <div className="image-link absolute bottom-2 left-2 flex rounded-full border border-solid border-gray-100 bg-white shadow-md p-1">
-                <TbLink size={20} />
-              </div>
-            </div>
-          )}
-          <div className="flex flex-col gap-2 overflow-x-hidden overflow-y-auto">
-            {value?.title && (
-              <p className="body2 font-medium">{value?.title}</p>
-            )}
-            {value?.description && (
-              <p className="body3 text-gray-400">{value?.description}</p>
-            )}
-          </div>
+          <CardContent
+            asset={asset}
+            width={width}
+            height={height}
+            breakpoint={breakpoint}
+          />
         </Link>
       ) : (
-        <div
-          className={`relative flex-1 ${value?.imageUrl ? 'grid' : ''}  grid-item-wrapper overflow-hidden p-3 gap-2`}
-          style={{
-            gridTemplateColumns:
-              width > height ? `${height}fr ${width - height}fr` : '',
-            gridTemplateRows:
-              // eslint-disable-next-line no-nested-ternary
-              width < height
-                ? `${height}fr ${height - width}fr`
-                : width === height
-                  ? '1fr 1fr'
-                  : '',
-          }}
-        >
-          {value?.imageUrl && (
-            <div className="relative w-full h-full rounded-xl overflow-hidden">
-              <Image
-                src={value?.imageUrl}
-                className="object-cover"
-                alt={`image_${id}`}
-                style={{
-                  objectPosition: `${value.pos?.[breakpoint] ? value.pos[breakpoint].x : 50}% ${value.pos?.[breakpoint] ? value.pos[breakpoint].y : 50}%`,
-                }}
-                quality={100}
-                fill
-                priority
-                sizes="(max-width: 842px) 100vw, 80vw"
-              />
-              {value.link && (
-                <a
-                  href={value.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="related-image-link"
-                  className="image-link absolute bottom-2 left-2 flex rounded-full border border-solid border-gray-100 bg-white shadow-md p-1"
-                >
-                  <TbLink size={20} />
-                </a>
-              )}
-            </div>
-          )}
-          <div className="flex flex-col gap-2 overflow-x-hidden	overflow-y-auto">
-            {value?.title && (
-              <p className="body2 font-medium">{value?.title}</p>
-            )}
-            {value?.description && (
-              <p className="body3 text-gray-400">{value?.description}</p>
-            )}
-          </div>
-        </div>
+        <CardContent
+          asset={asset}
+          width={width}
+          height={height}
+          breakpoint={breakpoint}
+        />
       )}
     </div>
   )
