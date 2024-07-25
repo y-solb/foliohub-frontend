986471f5858f4770355305fd52785c5be65db265
y-solb <ysolb99@gmail.com>
2024-07-25 18:59:35 +0900

    Feat: TextAsset 관련 스토리북 추가

---
+++ src/components/asset/TextAsset/index.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { TextAssetType } from '@/types'
+import TextAsset from '.'
+
+const meta: Meta<typeof TextAsset> = {
+  title: 'Asset/TextAsset',
+  component: TextAsset,
+  parameters: {
+    docs: {
+      subtitle: 'TextAsset은 텍스트를 보여주는 Asset입니다.',
+    },
+  },
+  argTypes: {
+    asset: {
+      description: 'TextAsset에 들어갈 내용을 설정합니다.',
+      table: {
+        type: { summary: 'TextAssetType' },
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div className="flex w-40 h-40">
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+export default meta
+type Story = StoryObj<typeof TextAsset>
+
+const mockAsset: TextAssetType = {
+  id: '123',
+  layoutId: 'abc',
+  type: 'content',
+  value: {
+    content: '<p>안녕하세요!</p>',
+  },
+}
+
+export const Default: Story = {
+  args: {
+    asset: mockAsset,
+  },
+}
